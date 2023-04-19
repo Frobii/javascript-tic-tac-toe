a742d33fae15068612df2ea28dad991e24016c0b
frobee <frobi.webdev@outlook.com>
2023-04-19 16:35:54 +0930

    change-dummy-grid-class-names-to-suit-js-function

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const gameboard = (() => {
   const grid = Array.from(Array(3), () => new Array(3));
+
   return { grid };
 })();
 
-const displayController = (() => {})();
+const displayController = (() => {
+  const positions = [];
+
+  for (let i = 0; i < 9; i += 1) {
+    positions.push(document.querySelector(`.pos-${i}`));
+  }
+
+  return { positions };
+})();
 
 console.log(gameboard.grid);
+console.log(displayController.positions);
