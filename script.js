af8a5f939b19e503a265dde64b267c4b5d6226b0
frobee <frobi.webdev@outlook.com>
2023-04-19 17:00:08 +0930

    create-a-function-which-populates-the-display

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const gameboard = (() => {
   const grid = Array.from(Array(3), () => new Array(3));
+  grid[0][2] = 'X';
+  grid[1][1] = 'O';
+  grid[2][0] = 'X';
 
   return { grid };
 })();
@@ -11,8 +14,15 @@ const displayController = (() => {
     positions.push(document.querySelector(`.pos-${i}`));
   }
 
-  return { positions };
+  function populateDisplay(board) {
+    for (let i = 0; i < positions.length; i += 1) {
+      const row = Math.floor(i / 3);
+      const col = i % 3;
+      positions[i].textContent = board[row][col];
+    }
+  }
+
+  return { positions, populateDisplay };
 })();
 
-console.log(gameboard.grid);
-console.log(displayController.positions);
+displayController.populateDisplay(gameboard.grid);
