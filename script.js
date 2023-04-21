e7480d853e043bde527a1c42a5f88a63208bc89c
frobee <frobi.webdev@outlook.com>
2023-04-21 14:27:41 +0930

    make-grid-elements-100-percent-of-their-cell-dimensions

--- script.js
+++ script.js
@@ -1,42 +1,56 @@
-const gameboard = (() => {
-  const gamePositons = Array.from(Array(3), () => new Array(3));
-
-  const domPositions = [];
-  for (let i = 0; i < 9; i += 1) {
-    domPositions.push(document.querySelector(`.pos-${i}`));
-  }
-
-  gamePositons[0][2] = 'X';
-  gamePositons[1][1] = 'O';
-  gamePositons[2][0] = 'X';
-
-  return { gamePositons, domPositions };
-})();
-
-const displayController = (() => {
-  function populateDisplay(boardArray, domArray) {
-    for (let i = 0; i < domArray.length; i += 1) {
-      const row = Math.floor(i / 3);
-      const col = i % 3;
-      domArray[i].textContent = boardArray[row][col];
-    }
-  }
-
-  return { populateDisplay };
-})();
-
-const createPlayer = (playerSymbol) => {
-  const symbol = playerSymbol;
+const player = () => {
+  const O = 'O';
+  const X = 'X';
 
   return {
-    symbol,
+    O,
+    X,
   };
 };
 
-const playerOne = createPlayer('O');
-const playerTwo = createPlayer('X');
+const playerO = player().O;
+const playerX = player().X;
 
-displayController.populateDisplay(
-  gameboard.gamePositons,
-  gameboard.domPositions
-);
+const gameLogic = () => {
+  function makeMove(event) {
+    let countO = 0;
+    let countX = 0;
+
+    for (let i = 0; i < 9; i += 1) {
+      if (gameboard.domPositions[i].textContent === 'O') {
+        countO += 1;
+      } else if (gameboard.domPositions[i].textContent === 'X') {
+        countX += 1;
+      }
+    }
+
+    if (event.target.textContent !== '') {
+      // change the color of the text to red for a second?
+    } else if (countO === 0 && countX === 0) {
+      // X always goes first
+      event.target.textContent = playerX;
+    } else if (countO < countX) {
+      event.target.textContent = playerO;
+    } else if (countX === countO) {
+      event.target.textContent = playerX;
+    }
+
+    countO = 0;
+    countX = 0;
+  }
+
+  return {
+    makeMove,
+  };
+};
+
+const gameboard = (() => {
+  const domPositions = [];
+
+  for (let i = 0; i < 9; i += 1) {
+    domPositions.push(document.querySelector(`.pos-${i}`));
+    domPositions[i].addEventListener('click', gameLogic().makeMove);
+  }
+
+  return { domPositions };
+})();
